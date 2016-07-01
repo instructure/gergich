13af7934ebd5318c649b7a34cc510c7adc9e229b
bootstraponline <code@bootstraponline.com>
2016-07-01 14:04:25 -0400

    Add SwiftLint capture support

---
+++ lib/gergich/capture/swiftlint_capture.rb
@@ -0,0 +1,29 @@
+module Gergich
+  module Capture
+    class SwiftlintCapture < BaseCapture
+      # See SeverityLevelsConfiguration.swift
+      SEVERITY_MAP = {
+        "error" => "error",  # console description
+        "warning" => "warn",
+        "w" => "warn",       # short console description
+        "w/e" => "error"
+      }.freeze
+
+      def run(output)
+        # rubocop:disable Metrics/LineLength
+        #
+        # Example:
+        # /path/to/My.swift:13:22: warning: Colon Violation: Colons should be next to the identifier when specifying a type. (colon)
+        # /path/to/Fail.swift:80: warning: Line Length Violation: Line should be 100 characters or less: currently 108 characters (line_length)
+        pattern = /
+          ^([^:\n]+):(\d+)(?::\d+)?:\s(\w+):\s(.*?)\n
+        /mx
+
+        output.scan(pattern).map { |file, line, severity, error, _context|
+          { path: file, message: "[swiftlint] #{error}",
+            position: line.to_i, severity: SEVERITY_MAP[severity] }
+        }.compact
+      end
+    end
+  end
+end
