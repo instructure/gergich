261878646b95ea21caac5038eb3bcdff1870242f
jgramoll <jgramoll@instructure.com>
2019-02-06 09:04:32 -0700

    strip whitespace around comment filepath

--- lib/gergich.rb
+++ lib/gergich.rb
@@ -450,13 +450,14 @@ module Gergich
     #            severe comment will be used to determine the overall
     #            Code-Review score (0, -1, or -2 respectively)
     def add_comment(path, position, message, severity)
+      stripped_path = path.strip
       raise GergichError, "invalid position `#{position}`" unless valid_position?(position)
       position = position.to_json if position.is_a?(Hash)
       raise GergichError, "invalid severity `#{severity}`" unless SEVERITY_MAP.key?(severity)
       raise GergichError, "no message specified" unless message.is_a?(String) && !message.empty?
 
       db.execute "INSERT INTO comments (path, position, message, severity) VALUES (?, ?, ?, ?)",
-                 [path, position, message, severity]
+                 [stripped_path, position, message, severity]
     end
 
     POSITION_KEYS = %w[end_character end_line start_character start_line].freeze
